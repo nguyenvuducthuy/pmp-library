618d88b5f6352e38a887948d2762ea826d2bbfde
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-20 17:18:50 +0000

    Deploy www.pmp-library.org to github.com/pmp-library/pmp-library.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,21 @@
+var NAVTREE =
+[
+  [ "pmp-library", "index.html", [
+    [ "User Guide", "userguide.html", "userguide" ],
+    [ "Bibliography", "citelist.html", null ],
+    [ "API Reference", "usergroup0.html", [
+      [ "Modules", "modules.html", "modules" ],
+      [ "Classes", "annotated.html", "annotated" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classpmp_1_1_surface_mesh.html#ab76e687f8e2ba74cb6d5918fc43caf36",
+"group__algorithms.html#gacc7a666bac3ee8f6003b80214e4c4b38"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
